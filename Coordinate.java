b3a94ece7bfe9d80ba848f42adb85cc0393d8365
Robert Larson <tesselwait@users.noreply.github.com>
2018-09-24 15:03:08 -0400

    Create Coordinate.java

---
+++ Coordinate.java
@@ -0,0 +1,17 @@
+public class Coordinate {
+	private int x;
+	private int y;
+
+	public Coordinate(int a, int b) {
+		x=a;
+		y=b;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getX() {
+		return x;
+	}
+}
